c7190a929921f881886169a57d22b0b7e56db62b
astrosalinas <astrosalinas35@gmail.com>
2019-04-19 15:28:48 -0400

    autentication user

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,27 @@
+const { User } = require('../models')
+
+class SessionController {
+  async create (req, res) {
+    return res.render('auth/sigin')
+  }
+
+  async store (req, res) {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      console.log('user no')
+      return res.redirect('/')
+    }
+
+    if (!(await user.checkPassword(password))) {
+      console.log('contrasenha incorrecta')
+      return res.redirect('/')
+    }
+
+    return res.redirect('app/dashboard')
+  }
+}
+
+module.exports = new SessionController()
